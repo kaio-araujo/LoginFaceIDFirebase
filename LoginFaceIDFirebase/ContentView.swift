39f0afe63be311d76a034d38e1a420903a50db12
Kaio <kaioaguanais@gmail.com>
2020-10-09 14:40:51 -0700

    Initial Commit

---
+++ LoginFaceIDFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginFaceIDFirebase
+//
+//  Created by Kaio Guanais on 2020-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginFaceIDFirebase/LoginFaceIDFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFaceIDFirebaseApp.swift
+//  LoginFaceIDFirebase
+//
+//  Created by Kaio Guanais on 2020-10-09.
+//
+
+import SwiftUI
+
+@main
+struct LoginFaceIDFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
